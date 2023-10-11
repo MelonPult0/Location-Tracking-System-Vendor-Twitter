0760362fa734bdd14736d107bc681eb3cb83700c
MelonPult0 <tmx9903@gmail.com>
2023-10-11 16:07:12 -0500

    Add method to update tweet field in dynamoDB

--- src/aws.ts
+++ src/aws.ts
@@ -1,6 +1,7 @@
-import { unmarshall } from '@aws-sdk/util-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
+import { TweetFormatted } from './types/twitter';
 
 dotenv.config();
 
@@ -20,7 +21,7 @@ export const dynamodbDescribeTable = async (tableName: string) => {
     return res;
   } catch (e) {
     if (e instanceof Error) {
-      return e;
+      throw e;
     }
     console.error(e);
     throw new Error('dynamodbDescribeTable error');
@@ -61,5 +62,77 @@ export const dynamodbScanTable = async function* (
       }
       throw new Error('dynamodbScanTable error');
     }
+  }
+};
+
+// 3 - Get All Scan Results
+export const getAllScanResults = async <T>(
+  tableName: string,
+  limit: number = 25
+) => {
+  try {
+    await dynamodbDescribeTable(tableName);
+    const scanTableGen = await dynamodbScanTable(tableName, limit);
+
+    const res: T[] = [];
+
+    let isDone = false;
+
+    while (!isDone) {
+      const iterator = await scanTableGen.next();
+
+      if (!iterator) {
+        throw new Error('No iterator returned');
+      }
+
+      if (iterator.done || !iterator.value.LastEvaluatedKey) {
+        isDone = true;
+      }
+
+      if (iterator.value) {
+        iterator.value.Items!.forEach((item: any) => res.push(item));
+      }
+    }
+
+    return res;
+  } catch (e) {
+    if (e instanceof Error) {
+      throw e;
+    }
+    throw new Error('getAllScanResults unexpected error');
+  }
+};
+
+// 4 - Update Tweet field
+export const dynamodbUpdateTweet = async (
+  tableName: string,
+  tweet: TweetFormatted,
+  twitterId: string
+) => {
+  try {
+    const params: AWS.DynamoDB.UpdateItemInput = {
+      TableName: tableName,
+      Key: marshall({ twitterId: twitterId }),
+      UpdateExpression:
+        'set #tweets = list_append(if_not_exists(#tweets, :empty_list), :tweet), #updated = :updated',
+      ExpressionAttributeNames: {
+        '#tweets': 'tweets',
+        '#updated': 'updated',
+      },
+      ExpressionAttributeValues: marshall({
+        ':tweet': [tweet],
+        ':updated': Date.now(),
+        ':empty_list': [],
+      }),
+    };
+
+    const result = await dynamodb.updateItem(params).promise();
+    console.log('Tweet added to record!');
+    return result;
+  } catch (e) {
+    if (e instanceof Error) {
+      throw e;
+    }
+    throw new Error('dynamodbUpdateTweet error object unknown type');
   }
 };
